5f98982502465d7bc1507632913063064561eec2
Theo Tran <theotran@Theos-MacBook-Pro.local>
2021-03-05 04:19:29 -1000

    Successfully parsed and sorted by dates.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { messages } from "./data.json";
 
 function App() {
+  const sortedMessages = messages.sort(function (x, y) {
+    return Date.parse(x.sentAt) - Date.parse(y.sentAt);
+  });
+  console.log("messages", messages);
+  console.log("sorted messages", sortedMessages);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MODERN HEALTH TAKE HOME ASSIGNMENT</h1>
     </div>
   );
 }
